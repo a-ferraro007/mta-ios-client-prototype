aab609837b8e6956be5d8b055d9667f0310eaac1
Anthony <adferra24@gmail.com>
2022-07-03 00:16:23 -0400

    Initial Commit

---
+++ transit-app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  transit-app
+//
+//  Created by Anthony Ferraro on 7/3/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ transit-app/transit_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  transit_appApp.swift
+//  transit-app
+//
+//  Created by Anthony Ferraro on 7/3/22.
+//
+
+import SwiftUI
+
+@main
+struct transit_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
